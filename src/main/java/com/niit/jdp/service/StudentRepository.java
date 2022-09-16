77b89b6b9ca0378c87ee6f62ab37055198835078
M V K <krishna.mugala9@gmail.com>
2022-09-16 18:42:46 +0530

    created a statement object

--- src/main/java/com/niit/jdp/service/StudentRepository.java
+++ src/main/java/com/niit/jdp/service/StudentRepository.java
@@ -12,12 +12,15 @@ import com.niit.jdp.model.Student;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepository {
     public List<Student> getAllStudentDetails(Connection connection) throws SQLException {
         String readQuery = "SELECT * FROM `sales_commission`.`sales_person`;";
-
+        List<Student> salespersonList = new ArrayList<>();
+        Statement statement = connection.createStatement();
         return null;
     }
 }
