a20620fabb33f5a52c26795bbf0da087ca8b05f6
M V K <krishna.mugala9@gmail.com>
2022-09-16 18:28:04 +0530

    created the studentRepository class in the repository package

---
+++ src/main/java/com/niit/jdp/service/StudentRepository.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 16-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.niit.jdp.service;
+
+public class StudentRepository {
+}
